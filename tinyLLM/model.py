1e7ee72dcc3df46e06e31fd97800ff0e17dd9b39
hexhowells <58524460+hexhowells@users.noreply.github.com>
2026-08-16 20:12:58 +0100

    remove weight sharing between embedding layers

--- tinyLLM/model.py
+++ tinyLLM/model.py
@@ -135,9 +135,6 @@ class GPT(nn.Module):
         ))
         self.lm_head = nn.Linear(config['n_embd'], config['vocab_size'], bias=False)
 
-        # weight sharing as per https://arxiv.org/abs/1608.05859
-        self.transformer.wte.weight = self.lm_head.weight  # type: ignore
-
         # init all weights, and apply a special scaled init to the residual projections, per GPT-2 paper
         self.apply(self._init_weights)
         for pn, p in self.named_parameters():
