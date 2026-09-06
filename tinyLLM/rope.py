e9d1e4c3a169c3ea31402f97d0c0d1a84d053d3d
hexhowells <morganhowells314@gmail.com>
2026-09-06 16:46:11 +0100

    fix wrong dim in _rotate_half

--- tinyLLM/rope.py
+++ tinyLLM/rope.py
@@ -38,7 +38,7 @@ def _rotate_half(x: torch.Tensor) -> torch.Tensor:
     x1 = x[..., :x.shape[-1] // 2]
     x2 = x[..., x.shape[-1] // 2:]
 
-    return torch.cat((-x2, x1), dim=1)
+    return torch.cat((-x2, x1), dim=-1)
 
 
 def apply_rotary_pos_emb(
