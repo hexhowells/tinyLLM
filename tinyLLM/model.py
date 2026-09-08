7131067234ee571a874cd04b49e29b7eb8c92d56
hexhowells <morganhowells314@gmail.com>
2026-09-08 22:45:07 +0100

    update activation function variable name

--- tinyLLM/model.py
+++ tinyLLM/model.py
@@ -92,13 +92,13 @@ class MLP(nn.Module):
     def __init__(self, config: dict):
         super().__init__()
         self.c_fc = nn.Linear(config['n_embd'], 4 * config['n_embd'])
-        self.gelu = ReLUSquared()
+        self.relu_sqrd = ReLUSquared()
         self.c_proj = nn.Linear(4 * config['n_embd'], config['n_embd'])
         self.dropout = nn.Dropout(config['resid_pdrop'])
 
     def forward(self, x):
         x = self.c_fc(x)
-        x = self.gelu(x)
+        x = self.relu_sqrd(x)
         x = self.c_proj(x)
         x = self.dropout(x)
         return x
