67b5b0b64a53a8bf388c0c5e38d43d852875ba5e
hexhowells <morganhowells314@gmail.com>
2026-09-09 01:19:36 +0100

    fix wrong indentation - saving checkpoint every batch...

--- train.py
+++ train.py
@@ -158,11 +158,11 @@ for step, batch in enumerate(loader):
 
         global_step += 1
 
-    # save final model
-    raw_model = model._orig_mod if hasattr(model, "_orig_mod") else model
-    torch.save({
-        'global_step': global_step,
-        'model_state_dict': raw_model.state_dict(),
-        'optimizer_state_dict': optimiser.state_dict(),
-        'config': config,
-    }, "checkpoints/gpt2_final.pt")
+# save final model
+raw_model = model._orig_mod if hasattr(model, "_orig_mod") else model
+torch.save({
+    'global_step': global_step,
+    'model_state_dict': raw_model.state_dict(),
+    'optimizer_state_dict': optimiser.state_dict(),
+    'config': config,
+}, "checkpoints/tinyllm.pt")
